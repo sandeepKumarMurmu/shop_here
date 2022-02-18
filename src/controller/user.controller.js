d30e7fbbc5bfd8b9082467d1a3e3f1ed0c714cad
sandeep kumar murmu <sandeep.16murmu@gmail.com>
2022-02-18 15:46:07 +0530

    half

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -6,28 +6,29 @@ const User = require("../models/user.model");
 const { mail_user, mail_admin } = require("../utils/transport.email");
 
 router.post(
-  "/new",
-  body("email").isEmail().withMessage("please enter the valid mail"),
-  body("full_name")
-    .isLength({ min: 2 })
-    .withMessage("name of minimum length of 2 characters")
-    .bail(),
-  async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
+    "/new",
+    body("email").isEmail().withMessage("please enter the valid mail"),
+    body("full_name")
+        .isLength({ min: 2 })
+        .withMessage("name of minimum length of 2 characters"),
+    body("password").isLength({ min: 8 }).withMessage("password min length is 8"),
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            console.log(error)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-      const data = await User.create(req.body);
-      const user_data = await User.find().lean().exec();
-      mail_user(data, user_data);
-      mail_admin(data, user_data);
-      return res.send(data);
-    } catch (er) {
-      return res.send(er.message);
-    }
-  },
+            const data = await User.create(req.body);
+            const user_data = await User.find().lean().exec();
+            mail_user(data, user_data);
+            mail_admin(data, user_data);
+            return res.send(data);
+        } catch (er) {
+            return res.send(er.message);
+        }
+    },
 );
 
 module.exports = router;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     full_name: { type: String, required: true },
     email: { type: String, required: true },
-    role: [{ type: String }]
+    password: { type: String, required: true },
+    role: [{ type: String, default: "customer" }]
 
 }, {
     versionKey: false,
