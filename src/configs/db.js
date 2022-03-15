59577219a793b0a9f0c9699e7ce55becf15212df
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-03-15 21:54:37 +0530

    done

--- src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = () => {
-    return mongoose.connect(
-        "mongodb+srv://sandeep:sandeep_123@web-14.21u1b.mongodb.net/shop_here?retryWrites=true&w=majority",
-    );
-};
