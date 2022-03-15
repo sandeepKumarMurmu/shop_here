37ba94b19d5c254ba571ed07f767404ec406c5bd
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-03-15 17:30:46 +0530

    comit
    sfd

---
+++ check/src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import { App } from "../App";
+
+function Main() {
+  return (
+    <div className="App">
+      {/* <App /> */}
+      <h1>connected</h1>
+    </div>
+  );
+}
+
+export default Main;
